35fe98ff7c1fb78aca92a84586ebe265f6a2617b
Punn Chunwimaleung <khunpunn2546@gmail.com>
2024-04-11 13:31:20 +0700

    done lab 6

--- src/main/java/ku/kinkao/entity/Member.java
+++ src/main/java/ku/kinkao/entity/Member.java
@@ -1,11 +1,14 @@
 package ku.kinkao.entity;
 
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 import java.util.UUID;
 import java.time.Instant;
+import ku.kinkao.config.AttributeEncryptor;
+
 
 @Data
 @Entity
@@ -14,11 +17,14 @@ public class Member {
     @Id
     @GeneratedValue
     private UUID id;
-
+    @Convert(converter = AttributeEncryptor.class)
     private String username;
     private String password;
+    @Convert(converter = AttributeEncryptor.class)
     private String firstName;
+    @Convert(converter = AttributeEncryptor.class)
     private String lastName;
+    @Convert(converter = AttributeEncryptor.class)
     private String email;
     private Instant createdAt;
     private String role;
